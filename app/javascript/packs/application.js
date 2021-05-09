f02c9b3c0469e29136a4964e8a53901525fd1cfb
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-05-09 13:33:52 +0900

    Add navigation menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// Semantic UI
+require("semantic-ui-sass")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "navigation"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// Added for Semantic UI
-require("semantic-ui-sass")
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
-module.exports = environment
-
-
-
 // Added for Semantic UI
 const webpack = require("webpack")
 
+module.exports = environment
+
+// Added for Semantic UI
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
