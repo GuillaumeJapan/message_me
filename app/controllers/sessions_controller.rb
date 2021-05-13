d4521149e0ffac2b65cc843315825a91d2095994
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-05-13 10:44:28 +0900

    Start authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,15 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:danger] = "pas bon"
+      render :new
+    end
+  end
+
 end
