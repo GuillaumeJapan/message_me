4ff35a06eca2bd25d207fa848996e970013489e5
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-05-14 15:16:58 +0900

    Set up channel, broadcast messages and render them

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,20 @@ class MessagesController < ApplicationController
   before_action :require_user
 
   def create
-    message = current_user.messages.build(message_params)
-    if message.save
-      redirect_to root_path
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'chatroom_channel',
+                                    { content: format_message(@message) }
     end
   end
 
   private
     def message_params
       params.require(:message).permit(:body)
+    end
+
+    def format_message(mess) 
+      render(partial: 'message', locals: {message: mess})  # message est la variable utilisée dans le partial _message.html.erb
     end
 
 end
---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,14 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    $('#message-container').append(data.content)
+  }
+});
